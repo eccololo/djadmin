4904fdc0b6535c4ae55f4732f204cedda76ffc06
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2025-02-05 13:55:43 +0100

    Add admin panel titles customization.

--- crm/admin.py
+++ crm/admin.py
@@ -5,4 +5,9 @@ from .models import Membership
 admin.site.unregister(User)
 admin.site.unregister(Group)
 
+# Customizing admin titles
+admin.site.site_header = "DjAdmin"
+admin.site.index_title = "Admin"
+admin.site.site_title = "DjAdmin"
+
 admin.site.register(Membership)
