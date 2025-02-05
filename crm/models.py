e7c88f2f3764665079e51986add2ab67daa4f9d8
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2025-02-05 13:27:59 +0100

    Add Membership model.

--- crm/models.py
+++ crm/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Membership(models.Model):
+
+    name = models.CharField(max_length=500)
+
+    MEMBERSHIP_CHOICES = (
+        ("s", "Standard"),
+        ("p", "Premium"),
+        ("ux", "Ultimate Delux"),
+    )
+
+    membership_plan = models.CharField(max_length=2, choices=MEMBERSHIP_CHOICES)
+    membership_active =models.BooleanField(default=True)
+    unique_code = models.CharField(max_length=250)
