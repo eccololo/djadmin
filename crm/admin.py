7efbdc013799cb01b10b8469c8b59f55b5e63236
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2025-02-05 13:39:26 +0100

    Add Membership model to admin panel. Remove User and Group model from admin panel.

--- crm/admin.py
+++ crm/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
+from .models import Membership
 
-# Register your models here.
+admin.site.unregister(User)
+admin.site.unregister(Group)
+
+admin.site.register(Membership)
