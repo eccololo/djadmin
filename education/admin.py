0e749dfe112d8b0ef62a88a57114ae3aeb64d45d
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2025-02-10 19:45:36 +0100

    Register Course and Lecture model in admin.py.

--- education/admin.py
+++ education/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Course, Lecture
 
-# Register your models here.
+class CourseAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = { "slug": ("course_title", ) }
+
+
+admin.site.register(Course, CourseAdmin)
+
+class LectureAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = { "slug": ("lecture_name", ) }
+
+
+admin.site.register(Lecture, LectureAdmin)
