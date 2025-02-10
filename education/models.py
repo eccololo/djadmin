b9c47f0ae112220e20643ec879eb9d69360d2854
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2025-02-10 19:32:10 +0100

    Create Course and Lecture model.

--- education/models.py
+++ education/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+
+    course_title = models.CharField(max_length=100)
+    course_description = models.TextField(max_length=1000)
+    slug = models.SlugField(max_length=100)
+
+
+class Lecture(models.Model):
+
+    lecture_name = models.CharField(max_length=100)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    slug = models.SlugField(max_length=100)
