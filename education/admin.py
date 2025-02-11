fbac02f52709a4b131449a8db476ecd6b36d406e
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2025-02-11 02:22:53 +0100

    Add static files configuration. Add custom login page template to admin panel.

--- education/admin.py
+++ education/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models import Course, Lecture
 
 
+class AdminLoginArea(admin.AdminSite):
+
+    login_template = "admin/login.html"
+
+
 class InlineLecture(admin.StackedInline):
 
     model = Lecture
