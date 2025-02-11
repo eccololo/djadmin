4974ebe00cd69fe343cf65f44ea28cdd19a71ec6
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2025-02-11 01:36:03 +0100

    Alter models in admin.py by inline model, fieldset etc.

--- education/admin.py
+++ education/admin.py
@@ -1,14 +1,41 @@
 from django.contrib import admin
 from .models import Course, Lecture
 
+
+class InlineLecture(admin.StackedInline):
+
+    model = Lecture
+    extra = 1
+    # max_num = 2
+
+
 class CourseAdmin(admin.ModelAdmin):
 
+    inlines = [InlineLecture]
+
     prepopulated_fields = { "slug": ("course_title", ) }
+
+    list_display = ["course_title", "course_description", "course_heading"]
+
+    def course_heading(self, obj):
+
+        return obj.course_title + " - " + obj.course_description
 
 
 admin.site.register(Course, CourseAdmin)
 
 class LectureAdmin(admin.ModelAdmin):
+
+    fieldsets = (
+        ("Lecture:", {
+            "fields": ("lecture_name", "slug"),
+            "description": "Lecture details"
+        }),
+        ("Course:", {
+            "fields": ("course",),
+            "description": "Course linked"
+        }),
+    )
 
     prepopulated_fields = { "slug": ("lecture_name", ) }
 
